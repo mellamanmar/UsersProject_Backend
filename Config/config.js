02bc54868a4728a2d8d6326b626ff6e5b247f708
mellamanmar <marimelissamc@gmail.com>
2023-08-22 14:51:49 -0500

    Update route in forum cotroller

---
+++ Config/config.js
@@ -0,0 +1,40 @@
+// // Importar las dependencias necesarias
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mysql = require('mysql2');
+
+// // Crear una instancia de Express
+// const app = express();
+// const port = 3000;
+
+// // Configurar el middleware para el manejo de datos en formato JSON
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // Configurar la conexión a la base de datos MySQL
+// const connection = mysql.createConnection({
+//     host: 'localhost', // Dirección del servidor MySQL
+//     user: 'tu_usuario', // Nombre de usuario de MySQL
+//     password: 'tu_contraseña', // Contraseña de MySQL
+//     database: 'baseanddata' // Nombre de la base de datos
+// });
+
+// // Conectar a la base de datos MySQL
+// connection.connect(err => {
+//     if (err) throw err;
+//     console.log('Conectado a la base de datos MySQL');
+// });
+
+// // Obtener todos los usuarios
+// app.get('/api/users', (req, res) => {
+//     connection.query('SELECT * FROM users', (err, results) => {
+//         if (err) throw err;
+//         res.json(results);
+//     });
+// });
+
+// // Crear un nuevo usuario
+// app.post('/api/users', (req, res) => {
+// module.exports = {
+//     DB
+// }
